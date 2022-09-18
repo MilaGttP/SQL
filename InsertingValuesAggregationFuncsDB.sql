faaaeab207962dd307b366bdf22827a73c71ccc2
Mila <101627933+MilaGttP@users.noreply.github.com>
2022-09-18 20:40:41 +0300

    Add files via upload

---
+++ InsertingValuesAggregationFuncsDB.sql
@@ -0,0 +1,40 @@
+
+INSERT INTO Subjects VALUES 
+('Math'),
+('English'),
+('Database Theory'),
+('Programming'),
+('Physics')
+
+INSERT INTO Teachers VALUES
+('Kirilo', 20000, 'Romanenko'),
+('Angelina', 21000, 'Klekot'),
+('Jack', 15000, 'Underhill'),
+('Dave', 17500, 'McQueen'),
+('Irina', 18000, 'Sergienko')
+
+INSERT INTO Faculties VALUES
+('Computer Science'),
+('Logistic Management'),
+('Cybersecurity')
+
+INSERT INTO Departments VALUES
+(120000, 'Software Development', 3),
+(100000, 'Management', 2)
+
+INSERT INTO Lectures VALUES
+(2, 'B103', 2, 1),
+(5, 'N786', 1, 3),
+(3, 'W132', 3, 4),
+(6, 'U371', 4, 2)
+
+INSERT INTO Groups VALUES
+('D201', 4, 1),
+('H109', 5, 2),
+('F129', 2, 1)
+
+INSERT INTO GroupsLectures VALUES
+(1, 3),
+(2, 1), 
+(3, 4),
+(1, 4)
