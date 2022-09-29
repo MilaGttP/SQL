df064a2dabac8637f0ca3597ac43323daf1a521f
Mila <101627933+MilaGttP@users.noreply.github.com>
2022-09-29 23:19:24 +0300

    Add files via upload

---
+++ InsertingValuesIntoBarberShopDB.sql
@@ -0,0 +1,38 @@
+
+INSERT INTO Services VALUES
+('Cut off hair', 400, '1,5'),
+('Wash head', 150, '0,3')
+
+INSERT INTO BarberShops VALUES
+('WithoutName', 4),
+('SinglePlace', 5),
+('Union', 2)
+
+INSERT INTO Barbers VALUES
+('Ivan', 'Sergienko', 22, 3, '01.06.2021', 1, 1, 1),
+('Gennadiy', 'Ovcharenko', 34, 10, '01.09.2010', 12, 1, 1),
+('Evgeniy', 'Zhuk', 24, 1, '07.08.2022', 0.1, 1, 1)
+
+INSERT INTO BarbersSchedules VALUES
+('2022-09-23', 3, 1),
+('2022-09-23', 3, 2),
+('2022-09-23', 3, 3)
+
+INSERT INTO Clients VALUES
+('Ivan', 2),
+('Sergiy', 1),
+('Jora', 3),
+('Fil', 2),
+('Roma', 1)
+
+INSERT INTO FeedBacks VALUES
+(5, 'The best barber!', 1, 2),
+(4, 'Good', 5, 1),
+(5, 'Amazing!', 4, 2),
+(3, 'Practice makes perfect!', 3, 1),
+(3, 'So - so', 2, 1)
+
+INSERT INTO ClientServices VALUES
+(2, 1),
+(1, 1),
+(4, 1)
