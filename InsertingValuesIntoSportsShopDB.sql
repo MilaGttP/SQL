27eefff3c0f0c1dcd6966ab605ef342c7ebc1e2d
Mila <101627933+MilaGttP@users.noreply.github.com>
2022-09-30 11:41:41 +0300

    Add files via upload

---
+++ InsertingValuesIntoSportsShopDB.sql
@@ -0,0 +1,20 @@
+
+INSERT INTO Goods VALUES
+('Rollerskates', '20271001', 23, 3400), 
+('Skateboards', '20251010', 12, 2600),
+('Bicycles', '20291213', 45, 8900)
+
+INSERT INTO Sellers VALUES
+('Roman', 'Andrienko', 27, '20190908', 16000),
+('Irina', 'Zhuk', 36, '20200202', 25600),
+('Andriy', 'Sheptiy', 59, '20011007', 21000)
+
+INSERT INTO Customers VALUES
+('Karine', 'Tiy', 32, '+380962345671', 'kT@gmail.com'),
+('Anjey', 'Bondar', 56, '+380982315681', 'ABon@gmail.com'),
+('Grigoriy', 'Ovchenko', 22, '+380972372197', 'GOvec@gmail.com'),
+('Nina', 'Sergj', 25, '+380984851297', 'NSer@gmail.com'),
+('Ivan', 'Menj', 67, '+380694853679', 'IMe@gmail.com')
+
+INSERT INTO Storages VALUES
+('StNum1', 'Soborna 11', 2)
